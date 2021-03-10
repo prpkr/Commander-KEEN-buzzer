1f4e6ee58312329797fd3d2abf63d998ce6c9a39
prpkr <56441900+prpkr@users.noreply.github.com>
2021-03-10 12:28:08 +0200

    Add files via upload

--- keen.ino
+++ keen.ino
@@ -1,124 +1,131 @@
 
 
 
+//COMPONENTS: adxl335 + Arduino + buzzer
 
-/* This part is for ADXL335   http://wiring.org.co/learning/basics/accelerometer.html
+int buzzerPin = 5;
 
-COMPONENTS: adxl335 + Arduino Pro Mini / ATmega328 + buzzer
+double x, y, z;
+double vect = 0;
 
-int x, y, z;
+int x_zero_G = 513;   // 399 - 627 min ja max
+int x_one_G = 114;
+int y_zero_G = 500;   //386 - 615
+int y_one_G = 114;
+int z_zero_G = 516;   //414 -618
+int z_one_G = 102;
 
 void setup()
 {
+  // Set the buzzer pin as an OUTPUT
+  pinMode(buzzerPin, OUTPUT);
+  digitalWrite(buzzerPin, LOW);
+
+  // initialize digital pin LED_BUILTIN as an output.
+  pinMode(LED_BUILTIN, OUTPUT);
+  
+  //analogReference(EXTERNAL);
+  pinMode(A0, INPUT);
+  pinMode(A1, INPUT);
+  pinMode(A2, INPUT);
   Serial.begin(9600);      // sets the serial port to 9600
+  digitalWrite(LED_BUILTIN, HIGH);
+
+  
 }
 
-void loop()
-{
-  x = analogRead(0);       // read analog input pin 0
-  y = analogRead(1);       // read analog input pin 1
-  z = analogRead(2);       // read analog input pin 1
-  Serial.print("accelerations are x, y, z: ");
+void loop(){
+
+  readADXL();
+
+  if (vect > 1.400) playHyppy();
+  
+  printData();
+
+}
+
+
+
+void readADXL(){
+  x = analogRead(A0);       // read analog input pin 0
+  x = (x - x_zero_G)/ x_one_G;
+  
+  y = analogRead(A1);       // read analog input pin 1
+  y = (y - y_zero_G)/ y_one_G;
+   
+  z = analogRead(A2);       // read analog input pin 2
+  z = (z - z_zero_G)/ z_one_G;
+   
+  vect = sqrt(sq(x) + sq(y) + sq(z));
+  
+}
+
+void printData(){
   Serial.print(x, DEC);    // print the acceleration in the X axis
   Serial.print(" ");       // prints a space between the numbers
   Serial.print(y, DEC);    // print the acceleration in the Y axis
   Serial.print(" ");       // prints a space between the numbers
-  Serial.println(z, DEC);  // print the acceleration in the Z axis
-  delay(100);              // wait 100ms for next reading
-}
-
-*/
-
-
-
+  Serial.print(z, DEC);  // print the acceleration in the Z axis
+  Serial.print(" ");       // prints a space between the numbers
+  Serial.println(vect, DEC);
+  delay(50);              // wait
+  
+  }
 
 
-/******************************************************************************
-
-https://learn.sparkfun.com/tutorials/lilypad-buzzer-hookup-guide/all
-
-
-LilyPad Buzzer Example
-SparkFun Electronics
-
-This example code shows how use a button (or alligator clips) to trigger sounds
-with the LilyPad Buzzer.
-
-Buzzer connections:
-   * + pin to 5
-   * - to -
-
-******************************************************************************/
-
-//Hellou Arduino + buzzer
-//COMPONENTS: Arduino Pro Mini / ATmega328 + buzzer
-
-// Pin the buzzer is attached to
-int buzzerPin = 5;
-
-// Set a time in milliseconds for all delays
-int delayTime = 100; 
-
-// Notes
-const int C = 1046;
-const int D = 1175;
-const int E = 1319;
-const int F = 1397;
-const int G = 1568;
-const int A = 1760;
-const int B = 1976;
-const int C1 = 2093;
-const int D1 = 2349;
-
-void setup()
-{
-    // Set the buzzer pin as an OUTPUT
-    pinMode(buzzerPin, OUTPUT);
-
-      // initialize digital pin LED_BUILTIN as an output.
-  pinMode(LED_BUILTIN, OUTPUT);
-   Serial.begin(9600);
-
-}
-
-void loop()
-{
-    Serial.println("Play");
-    playSong();
+void playAskel(){          //eka on walk ääni
+  tone(buzzerPin, 233, 7);
+  delay(7);
+  tone(buzzerPin, 246, 7);
+  delay(250);
+  }
 
 
-    //blink for 10 seconds
-    for (int i = 0; i <= 5; i++) {
-    Serial.println("Blink");
-    digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
-    delay(1000);                       // wait for a second
-    digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
-    delay(1000);                       // wait for a second
-    }
-                    
-    
-}
+void playHyppy(){           //toka on hyppy
 
-void playSong() 
-{
-  /*
-  tone(buzzerPin, C);
-  delay(delayTime);
-  tone(buzzerPin, D);
-  delay(delayTime);
-  tone(buzzerPin, E);
-  delay(delayTime);
-  tone(buzzerPin, F);
-  delay(delayTime);
-  tone(buzzerPin, G);
-  delay(delayTime);
-  tone(buzzerPin, A);
-  delay(delayTime);
-  tone(buzzerPin, B);
-  delay(delayTime);
-  tone(buzzerPin, C1);
-  delay(delayTime); */
-  // Use noTone() to shut off the buzzer and delay to create a 'rest'
-  noTone(buzzerPin);
-  delay(delayTime); 
+tone(buzzerPin, 261, 10);
+delay(10);
+tone(buzzerPin, 261, 10);
+delay(10);
+tone(buzzerPin, 277, 7);
+delay(7);
+tone(buzzerPin, 277, 7);
+delay(7);
+tone(buzzerPin, 293, 7);
+delay(7);
+tone(buzzerPin, 293, 7);
+delay(7);
+tone(buzzerPin, 311, 7);
+delay(7);
+tone(buzzerPin, 311, 7);
+delay(7);
+tone(buzzerPin, 311, 7);
+delay(7);
+tone(buzzerPin, 311, 7);
+delay(7);
+tone(buzzerPin, 311, 7);
+delay(7);
+tone(buzzerPin, 311, 7);
+delay(7);
+tone(buzzerPin, 293, 7);
+delay(7);
+tone(buzzerPin, 293, 7);
+delay(7);
+tone(buzzerPin, 293, 7);
+delay(7);
+tone(buzzerPin, 311, 7);
+delay(7);
+tone(buzzerPin, 311, 7);
+delay(7);
+tone(buzzerPin, 329, 7);
+delay(7);
+tone(buzzerPin, 329, 7);
+delay(7);
+tone(buzzerPin, 329, 7);
+delay(7);
+tone(buzzerPin, 349, 7);
+delay(7);
+tone(buzzerPin, 329, 7);
+
+delay(200);
 }
